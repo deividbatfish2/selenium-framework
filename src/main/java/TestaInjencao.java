6164b10cd3932836f18d7ed77edd2f98e6d10a3c
Deivid Teixeira <deividbatfish2@gmail.com>
2018-10-25 23:42:26 -0300

    Primeiro exemplo com o webdriver sendo gerenciado e injetado pelo spring

---
+++ src/main/java/TestaInjencao.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class TestaInjencao {
+    private String baseUrl;
+
+
+
+    @FindBy(name = "q")
+    private WebElement txbSearch;
+
+    public void searchFor(String search) {
+        this.txbSearch.sendKeys(search);
+        this.txbSearch.submit();
+    }
+
+
+}
